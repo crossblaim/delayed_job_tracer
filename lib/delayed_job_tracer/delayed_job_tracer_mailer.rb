9f87c732bee1a8f6d8bb7740d79d1b1fb14f6933
Claudio Perez Gamayo <crossblaim@gmail.com>
2015-01-27 19:35:56 +0100

    Include the app name in the test email

--- lib/delayed_job_tracer/delayed_job_tracer_mailer.rb
+++ lib/delayed_job_tracer/delayed_job_tracer_mailer.rb
@@ -4,7 +4,7 @@ class DelayedJobTracerMailer < ActionMailer::Base
     config  = YAML.load_file(Rails.root.join('config', 'delayed_job_tracer_config.yml'))
     to      = config['monitor']['username']
     from    = config['alert']['email'] # TODO ActionMailer::Base.smtp_settings[:user_name]
-    subject = "[DelayedJobTracer] Test message sent at: #{Time.zone.now}"
+    subject = "[DelayedJobTracer][#{config['app']['name']}] Test message sent at: #{Time.zone.now}"
     message = 'This is a test message to ensure messages are being delivered successfully via delayed_job.'
     mail(:to => to, :from => from, :subject => subject) do |format|
       format.text { render :text => message }
