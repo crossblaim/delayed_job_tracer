7dbe5cdb7514d63d7c8f9c12c9a7c5d56de848de
Claudio Perez Gamayo <crossblaim@gmail.com>
2015-04-29 17:57:05 +0200

    Add return_path header to messages

--- lib/delayed_job_tracer/delayed_job_tracer_mailer.rb
+++ lib/delayed_job_tracer/delayed_job_tracer_mailer.rb
@@ -6,7 +6,7 @@ class DelayedJobTracerMailer < ActionMailer::Base
     from    = config['alert']['email'] # TODO ActionMailer::Base.smtp_settings[:user_name]
     subject = "[DelayedJobTracer][#{config['app']['name']}] Test message sent at: #{Time.zone.now}"
     message = 'This is a test message to ensure messages are being delivered successfully via delayed_job.'
-    mail(:to => to, :from => from, :subject => subject) do |format|
+    mail(:to => to, :from => from, :subject => subject, :return_path => from) do |format|
       format.text { render :text => message }
     end
   end
